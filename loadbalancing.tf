6b6b165ada6f6e2fade228c0e6befff630103358
Erza Duraku <erza.duraku@student.uni-pr.edu>
2026-06-11 16:51:51 +0200

    Add ALB, target group and outputs

---
+++ loadbalancing.tf
@@ -0,0 +1,36 @@
+resource "aws_lb_target_group" "main" {
+  name        = "${var.group_name}-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "instance"
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+    interval            = 10
+    timeout             = 5
+    path                = "/"
+    matcher             = "200"
+  }
+}
+
+resource "aws_lb" "main" {
+  name               = "${var.group_name}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = aws_subnet.public[*].id
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "alb_dns_name" {
+  value = aws_lb.main.dns_name
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
